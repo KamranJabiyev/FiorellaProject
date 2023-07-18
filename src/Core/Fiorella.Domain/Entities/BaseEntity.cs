e9cf1e0e6f1c30535cb5b27feab7e8df2604585a
Kamran Jabiyev <kamran.jabiyev@code.local>
2023-07-18 17:01:27 +0400

    Initial

---
+++ src/Core/Fiorella.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Fiorella.Domain.Entities;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ src/Presentation/Fiorella.API/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorella.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
