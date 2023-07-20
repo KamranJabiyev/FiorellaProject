63e3f92b082c852da974b212e9a22df455c83f63
Rahil Habibli <rahilah@code.edu.az>
2023-07-20 23:18:10 +0400

    BE-009 Category Fluent Validation Regex Check Added

--- src/Core/Fiorella.Aplication/Validators/CategoryValidators/CategoryCreateDtoValidator.cs
+++ src/Core/Fiorella.Aplication/Validators/CategoryValidators/CategoryCreateDtoValidator.cs
@@ -8,7 +8,10 @@ public class CategoryCreateDtoValidator : AbstractValidator<CategoryCreateDto>
 {
     public CategoryCreateDtoValidator()
     {
-        RuleFor(x => x.name).NotNull().NotEmpty().MaximumLength(30);
-        RuleFor(x => x.description).MaximumLength(500);
+        RuleFor(c => c.name).NotNull().NotEmpty().MaximumLength(30)
+            .Matches("^[a-zA-Z]+$").WithMessage("Name can only contain letters.");
+
+        RuleFor(c => c.description).MaximumLength(500).Matches("^[a-zA-Z0-9]+$")
+            .WithMessage("Description can only contain letters and digits (no symbols)."); 
     }
 }
