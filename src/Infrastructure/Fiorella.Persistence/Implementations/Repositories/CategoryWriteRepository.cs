feadfaf09e8551e660963bd86cc6331925e52a0a
KamranJabiyev <kamran.jabiyev@code.edu.az>
2023-07-20 16:32:01 +0400

    BE-009 created Category repositories

---
+++ src/Infrastructure/Fiorella.Persistence/Implementations/Repositories/CategoryWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fiorella.Aplication.Abstraction.Repository;
+using Fiorella.Domain.Entities;
+using Fiorella.Persistence.Contexts;
+
+namespace Fiorella.Persistence.Implementations.Repositories;
+
+public class CategoryWriteRepository : WriteRepository<Category>, ICategoryWriteRepository
+{
+    public CategoryWriteRepository(AppDbContext context) : base(context)
+    {
+    }
+}
