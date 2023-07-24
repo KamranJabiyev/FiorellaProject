c7317da618e51018e97b1a782c0a19dfd75c9ad3
KamranJabiyev <kamran.jabiyev@code.edu.az>
2023-07-24 16:15:35 +0400

    BE-010 added data initializer

---
+++ src/Infrastructure/Fiorella.Persistence/Contexts/AppDbContextInitializer.cs
@@ -0,0 +1,54 @@
+﻿using Fiorella.Domain.Entities;
+using Fiorella.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Fiorella.Persistence.Contexts;
+
+public class AppDbContextInitializer
+{
+    private readonly AppDbContext _context;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IConfiguration _configuration;
+
+    public AppDbContextInitializer(AppDbContext context,
+                                   UserManager<AppUser> userManager,
+                                   RoleManager<IdentityRole> roleManager,
+                                   IConfiguration configuration)
+    {
+        _context = context;
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _configuration = configuration;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _context.Database.MigrateAsync();
+    }
+
+    public async Task RoleSeedAsync()
+    {
+        foreach (var role in Enum.GetValues(typeof(Role)))
+        {
+            if (!await _roleManager.RoleExistsAsync(role.ToString()))
+            {
+                await _roleManager.CreateAsync(new() { Name = role.ToString() });
+            }
+        }
+    } 
+
+    public async Task UserSeedAsync()
+    {
+        AppUser appUser = new()
+        {
+            UserName = _configuration["SuperAdminSettings:username"],
+            Email = _configuration["SuperAdminSettings:email"]
+        };
+
+        await _userManager.CreateAsync(appUser, _configuration["SuperAdminSettings:password"]);
+        await _userManager.AddToRoleAsync(appUser,Role.SuperAdmin.ToString());
+    }
+}
--- src/Presentation/Fiorella.API/Program.cs
+++ src/Presentation/Fiorella.API/Program.cs
@@ -21,12 +21,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddPersistenceServices();
+builder.Services.AddScoped<AppDbContextInitializer>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var instance = scope.ServiceProvider.GetRequiredService<AppDbContextInitializer>();
+    await instance.InitializeAsync();
+    await instance.RoleSeedAsync();
+    await instance.UserSeedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
